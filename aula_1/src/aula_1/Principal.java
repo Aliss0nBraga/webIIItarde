a4cdee62ff1670f568df5f6ba3da1d9edff2215f
Anderson Choren <professorchoren@gmail.com>
2021-09-06 17:11:11 -0300

    Aula 1

---
+++ aula_1/src/aula_1/Principal.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package aula_1;
+
+/**
+ * @author professorchoren
+ *
+ */
+public class Principal {
+	public static void main(String[] args) {
+		String linguagem = "Java";
+		int versao = 11;
+		//System.out.print("Olá mundo em "+linguagem+" "+versao+"!!!");
+		System.out.printf("Olá mundo em %s %d!!!",linguagem,versao);
+	}
+}
